2e224b0f25e3cf86c24117553f6f9c93a3e9ac2e
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-01-27 09:00:03 -0600

    Add removeFromArray definition
    Remove elements provided as arguments using array slicing.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+    for (let item of toRemove){
+        index = array.indexOf(item);
+        if (index > -1){
+            array = array.slice(0,index).concat(array.slice(index+1));
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
