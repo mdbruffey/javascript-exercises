198b4c374510c97bcd9a8c46f2846e194ccfa23a
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-02-17 08:59:01 -0600

    Add function definition for palindromes()
    Works with random capitalization, removes punctuation and
    spacing.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let array = Array.from(string.toLowerCase().replaceAll(" ",""));
+    array = array.filter(char => char >= "A" && char <= "z");
+    string = array.join("");
+    array = array.reverse().join("");
+    return array === string;
 };
 
 // Do not edit below this line
