0ab8b41b01c5537d595e5a505ed3d89a39173988
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-02-17 08:20:14 -0600

    Add function definitions for various calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	count = 0;
+  for(num of numbers){
+    count += num;
+  }
+  return count;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((mult, num) => mult*num,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	return a <= 1 ? 1 : a * factorial(a-1);
 };
 
 // Do not edit below this line
