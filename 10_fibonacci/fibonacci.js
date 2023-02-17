9c076aa2b75b127e15e410e1bece39f11b5dc8b0
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-02-17 10:15:06 -0600

    Add definition for fibonacci() function
    Use Binet's formula to calculate nth term of the sequence.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0){
+        return "OOPS";
+    }
+    return Math.round((Math.pow(1.6180339,num) - Math.pow(-0.6180339,num))/2.236067977);
 };
 
 // Do not edit below this line
