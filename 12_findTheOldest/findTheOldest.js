5fa915cfe481d646f44f2cc5e74cdff5044a5317
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-02-17 10:45:30 -0600

    Add definition for findTheOldest() function
    finds the oldest person in a list. Uses the current year as the
    'yearOfDeath' for people who haven't died yet.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let old = {yearOfBirth: 0, yearOfDeath: 1};
+    return people.reduce((oldest, person) => {
+        if(!person["yearOfDeath"]){
+            person["yearOfDeath"] = new Date().getFullYear();
+        }
+        if (person.yearOfDeath - person.yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth){
+            return person;
+        }
+        return oldest
 
+    }, old);
 };
 
 // Do not edit below this line
